e8349f9de80fbf31aeda30a636257057fb16efd5
yazid_ha <63234062+yaz-gear@users.noreply.github.com>
2026-01-20 18:55:16 +0000

    Create macd.py

---
+++ indicators/macd.py
@@ -0,0 +1,7 @@
+from indicators.moving_average import ema
+
+def macd(series, fast=12, slow=26, signal=9):
+    macd_line = ema(series, fast) - ema(series, slow)
+    signal_line = ema(macd_line, signal)
+    histogram = macd_line - signal_line
+    return macd_line, signal_line, histogram
